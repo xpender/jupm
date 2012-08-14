ccde449921f8145736d4454ca797ca0466d1c27d
Marko Kercmar <m.kercmar@bigpoint.net>
2012-08-14 12:12:05 +0200

    more information on frontend list page

--- include/JuPm/Frontend/Page/List.php
+++ include/JuPm/Frontend/Page/List.php
@@ -9,13 +9,37 @@ class JuPm_Frontend_Page_List extends JuPm_Frontend_PageAbstract
 
         echo '<div class="main">' . "\n";
            
-        echo '<ul>';
+        echo '<table border="1">' . "\n";
+        echo '<tr>' . "\n";
+        echo '<th>Name</th>' . "\n";
+        echo '<th>Latest version</th>' . "\n";
+        echo '<th>Description</th>' . "\n";
+        echo '</tr>' . "\n";
             
         foreach ($oPackagesDb->allPackages() as $iPackageId => $sPackageName) {
-            echo '<li><a href="/?action=info&package=' . $sPackageName . '">' . $sPackageName . '</li>';
+            // get versions..
+            $aPackageVersions = $oPackagesDb->allPackageVersions($iPackageId);
+
+            // sort by version
+            uksort($aPackageVersions, array('JuPm_Helper_Version', 'sort'));
+
+            // get latests version for info..
+            $aTmp = $aPackageVersions;
+
+            $aLatestVersion = array_shift($aTmp);
+
+            unset($aTmp);
+
+            echo '<tr>' . "\n";
+
+            echo '<td><a href="/?action=info&package=' . $sPackageName . '">' . $sPackageName . '</a></td>' . "\n";
+            echo '<td>' . $aLatestVersion['version'] . '</td>' . "\n";
+            echo '<td>' . $aLatestVersion['description'] . '</td>' . "\n";
+            
+            echo '</tr>' . "\n";
         }
 
-        echo '</ul>';
+        echo '</table>' . "\n";
 
         echo '</div>' . "\n";
 
