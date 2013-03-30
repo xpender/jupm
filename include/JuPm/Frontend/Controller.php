695d3ffb0cdf02b91c70d8c4c107ff0c9ea59fdd
Marko Kercmar <m.kercmar@bigpoint.net>
2013-03-30 14:02:01 +0100

    Renames and first adjustments

--- include/JuPm/Frontend/Controller.php
+++ include/JuPm/Frontend/Controller.php
@@ -1,30 +1,21 @@
 <?php
+/**
+ * jupm
+ *
+ * @package net.xpender.jupm
+ * @author Marko Kercmar <m.kercmar@bigpoint.net>
+ */
 class JuPm_Frontend_Controller
 {
-    public function __construct()
+    public function dispatch($aRequest)
     {
-    }
-
-    public function dispatch()
-    {
-        if (isset($_REQUEST['action'])) {
-            $sAction = $_REQUEST['action'];
+        if (isset($aRequest['action'])) {
+            $sAction = $aRequest['action'];
         } else {
             $sAction = 'index';
         }
 
-        $aValidActions = array(
-            'list',
-            'info',
-            'index'
-            );
-
-        if (!in_array($sAction, $aValidActions)) {
-            $sAction = 'index';
-        }
-
-        $sClassName = 'JuPm_Frontend_Page_' . ucfirst($sAction);
-
-        new $sClassName();
+        $sClassName = 'JuPm_Frontend_Action_' . ucfirst($sAction);
+        new $sClassName;
     }
 }
--- www/index.php
+++ www/index.php
@@ -1,11 +1,23 @@
 <?php
+/**
+ * jupm
+ *
+ * @author Marko Kercmar <m.kercmar@bigpoint.net>
+ */
+
+ // include bootstrap
 require 'bootstrap.php';
 
+// check for packages db file
 if (!file_exists(JUPM_PACKAGES_DB)) {
     echo 'ERROR - no packages db!';
 
     exit;
 }
 
+// le controller does the magic
 $oFrontendController = new JuPm_Frontend_Controller();
-$oFrontendController->dispatch();
+
+$oFrontendController->dispatch(
+    $_REQUEST
+    );
